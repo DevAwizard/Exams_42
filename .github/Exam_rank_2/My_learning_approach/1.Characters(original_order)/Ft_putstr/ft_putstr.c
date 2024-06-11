720e4a888cbb1fa87997b316bdf5066bed96d888
Anita <devWizardA@gmail.com>
2024-06-11 15:22:38 +0200

    function ft_putstr

---
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Ft_putstr/ft_putstr.c
@@ -0,0 +1,28 @@
+// Always include Headlines, if you want to compile the program
+
+# include <unistd.h> // write 
+# include <stdio.h> // printf
+
+void ft_putstr(char *string)
+{
+	int index;
+
+	index = 0;
+
+	while (string[index] != '\0')
+	{
+		write (1, &string[index], 1);
+		index++;
+	}
+}
+// main program
+
+int main(void)
+{
+    char *test_string;
+
+	test_string = "Good Job pal";
+
+    ft_putstr(test_string);
+    return 0;
+}
