d2b52d9d844ef0556b92da66a31ebe37b547fb4f
Anita <devWizardA@gmail.com>
2024-06-11 22:45:26 +0200

    ft_strcspn function

---
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Ft_strcspn/ft_strcspn.c
@@ -0,0 +1,66 @@
+# include <stdio.h>
+#include <stddef.h>
+
+/*
+This program defines a function `ft_strcspn` that searches a string (`s1`) for the first occurrence of any character present in another string (`reject`), and returns the position of that character.
+
+Example:
+Input: s1 = "hola mundo", reject = "aeiou"
+Output: First occurrence of a rejected character in 'hola mundo': 1
+
+Input: s1 = "programación", reject = "aeiou"
+Output: First occurrence of a rejected character in 'programación': 2
+
+Input: s1 = "lenguaje", reject = "xyz"
+Output: First occurrence of a rejected character in 'lenguaje': none
+
+*/
+
+// Function that checks the two strings and finds the position where a rejected character first appears
+
+
+size_t ft_strcspn(const char *s1, const char *reject)
+{
+	int index_s1;
+	int index_reject;
+
+	index_s1 = 0;
+
+	while(s1[index_s1] != '\0')
+	{
+		index_reject = 0;
+		while (reject[index_reject] != '\0')
+		{
+			if (s1[index_s1] == reject[index_reject])
+				return(index_s1);
+			
+			index_reject++;
+		}
+        // If no reject characters are found in the string, return 0
+		if (reject[index_reject] == 0)
+			return (0);
+		index_s1++;
+	}
+	return (index_s1);
+}
+
+// Main program
+
+int main (void)
+{
+	char *s1;
+	char *reject;
+
+	s1 = "lenguaje";
+	reject = "xyz";
+
+	printf("%zu\n", ft_strcspn(s1, reject));
+	return (0);
+}
+
+
+
+
+
+
+
