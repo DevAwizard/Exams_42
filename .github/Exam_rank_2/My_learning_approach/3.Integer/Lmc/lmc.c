f64a9aab6e03644297500006562cc7e8dd9065e9
Anita <devWizardA@gmail.com>
2024-06-13 11:24:19 +0200

    lmc program

---
+++ .github/Exam_rank_2/My_learning_approach/3.Integer/Lmc/lmc.c
@@ -0,0 +1,42 @@
+# include <stdio.h>
+
+// Function that calculates the least common multiple (LCM) of two positive integers
+
+// Example lmc of 14, 20 is 140
+
+unsigned int lcm (unsigned int a, unsigned int b)
+{
+	unsigned int number;
+
+	number = 0;
+
+	if (a == 0 || b == 0)
+		return (0);
+	
+	if (a > b)
+		number = a;
+	else
+		number = b;
+
+	while (1)
+	{
+		if (number % a == 0 && number % b == 0)
+			return (number);
+		
+		number++;
+
+	}
+	return (0);
+}
+
+// Main program
+int main(void)
+{
+
+	int number;
+	
+	number = lcm(14, 20);
+    printf("%u\n", number);
+	
+    return 0; 
+}
