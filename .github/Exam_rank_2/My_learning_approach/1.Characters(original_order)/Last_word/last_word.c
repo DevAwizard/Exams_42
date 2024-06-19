257f65a914e593aaf7738ad898b29194a09026eb
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-06-19 08:53:36 +0200

    Update last_word.c

--- .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Last_word/last_word.c
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Last_word/last_word.c
@@ -20,38 +20,34 @@ int main (int argc, char **argv)
 
 	if (argc != 2)
 	{
-		write (1, "\n", 1);
+		write(1, "\n", 1);
 		return (0);
 	}
+
 	index = 0;
 	string = argv[1];
 
+
 	while (string[index] != '\0')
 		index++;
-	
-	// Start from the last letter and not from the null character
+
 	index--;
 
-	// Skip spaces and tabs until reaching a non-space character
-	while (string[index] >= 0 && string[index] <= 32)
+	while (index >= 0 && (string[index] == ' ' || string[index] == '\t'))
+		index--;
+
+	while (index >= 0 && string[index] != ' ' && string[index] != '\t')
 		index--;
 	
-	// Move the index to the first letter of the word (last word)
-	while (string[index] >= 0 && string[index] > 32)
-		index--;
-
-	// Start from the first character of the last word
 	index++;
 
-	while(string[index] != '\0')
+	while (string[index] != '\0' && string[index] != ' ' && string[index] != '\t')
 	{
-		write(1, &string[index], 1);
+		write (1, &string[index], 1);
 		index++;
 	}
-
 	write (1, "\n", 1);
 	return (0);
 }
-
 
 
