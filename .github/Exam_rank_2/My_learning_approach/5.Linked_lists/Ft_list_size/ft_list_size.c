a2084faaa6216c992b20912612fc1dc488650a02
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-06-27 19:16:47 +0200

    Update ft_list_size.c

--- .github/Exam_rank_2/My_learning_approach/5.Linked_lists/Ft_list_size/ft_list_size.c
+++ .github/Exam_rank_2/My_learning_approach/5.Linked_lists/Ft_list_size/ft_list_size.c
@@ -4,7 +4,7 @@
 // Function to calculate the number of elements in a linked list
 int ft_list_size(t_list *begin_list)
 {
-    int size;  // Variable to hold the count of nodes in the list
+    int size;
 
     size = 0;
 
