1df06d5bbedf9c3de770df82eb6ece1f60464eaf
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-07-03 22:01:20 +0200

    Update and rename parasum.c to paramsum.c

--- .github/Exam_rank_2/My_learning_approach/3.Integer/Paramsum/paramsum.c
+++ .github/Exam_rank_2/My_learning_approach/3.Integer/Paramsum/paramsum.c
@@ -17,6 +17,13 @@ int main (int argc, char **argv)
 {
 	(void)argv;
 
+	if (argc < 2)
+	{
+	  write (1, "0", 1)
+          write (1, "\n", 1);
+	  return (0);
+	}
+
 	write_number(argc - 1);
 
 	write (1, "\n", 1);
