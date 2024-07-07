fc40f4128cc31d102d3be4e84eb21dac9b59063b
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-07-07 11:14:04 +0200

    Update str_capitalizer.c

--- .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Str_capitalizer/str_capitalizer.c
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Str_capitalizer/str_capitalizer.c
@@ -9,42 +9,62 @@ Input: "hello world! 42FOOBAR"
 Output: "Hello World! 42foobar"
 
 Input: "a SIMPLE test"
-Output: "A Simple Test" */
+Output: "A Simple Test" 
 
-void str_capitalizer(char *str)
+Input: "SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t" 
+
+Output:
+Second Test A Little Bit   Moar Complex$
+   But... This Is Not That Complex$
+     Okay, This Is The Last 1239809147801 But Not    The Least    T$
+
+*/
+
+#include <unistd.h>
+
+int ft_strlen(char *str)
 {
-    int index;
-	
-	index = 0;
-
-    while (str[index] != '\0')
+    int len = 0;
+    while (str[len] != '\0')
     {
-        if ((index == 0 || str[index - 1] == ' ' || str[index - 1] == '\t') && (str[index] >= 'a' && str[index] <= 'z'))
-            str[index] -= 32;
-        else if (!(index == 0 || str[index - 1] == ' ' || str[index - 1] == '\t') && (str[index] >= 'A' && str[index] <= 'Z'))
-            str[index] += 32;
-        write(1, &str[index], 1);
+        len++;
+    }
+    return len;
+}
+
+void str_capitalizer (char *string)
+{
+    int index = 0;
+    int start = 0;
+
+    while (string[start] == ' ' || (string[start] >= 9 && string[start] <= 13))
+        start++;
+
+    while (string[index] != '\0')
+    {
+        if ((index == start || string[index - 1] == ' ' || string[index - 1] == '\t') && (string[index] >= 'a' && string[index] <= 'z'))
+            string[index] -= 32;
+        else if (!(index == start || string[index - 1] == ' ' || string[index - 1] == '\t') && (string[index] >= 'A' && string[index] <= 'Z'))
+            string[index] += 32;
+
         index++;
     }
 }
 
-// Main program
-
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-    int index;
-	
-	index = 1;
+    int index = 1;
 
     if (argc < 2)
     {
-        write(1, "\n", 1);
+        write (1, "\n", 1);
         return (0);
     }
 
     while (index < argc)
     {
         str_capitalizer(argv[index]);
+        write(1, argv[index], ft_strlen(argv[index]));
         write(1, "\n", 1);
         index++;
     }
