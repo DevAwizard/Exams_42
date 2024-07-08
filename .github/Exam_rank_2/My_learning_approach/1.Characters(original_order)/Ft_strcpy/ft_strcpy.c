b1111cfd486dbaa4717ae6f721ad2878e0a58845
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-07-08 12:24:46 +0200

    Update ft_strcpy.c

--- .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Ft_strcpy/ft_strcpy.c
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Ft_strcpy/ft_strcpy.c
@@ -1,3 +1,17 @@
 char    *ft_strcpy(char *s1, char *s2)
 {
-  
+	int index;
+
+	index = 0;
+
+	while (s2[index] != '\0')
+	{
+		s1[index] = s2[index];
+		index++;
+	}
+
+	s1[index] = '\0';
+
+	return (s1);
+
+}
