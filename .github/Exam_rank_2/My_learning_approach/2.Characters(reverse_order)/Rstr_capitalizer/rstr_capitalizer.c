092ab8c6e0efd2658eb2c195b645e41afe924dea
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-07-09 13:24:16 +0200

    Update rstr_capitalizer.c

--- .github/Exam_rank_2/My_learning_approach/2.Characters(reverse_order)/Rstr_capitalizer/rstr_capitalizer.c
+++ .github/Exam_rank_2/My_learning_approach/2.Characters(reverse_order)/Rstr_capitalizer/rstr_capitalizer.c
@@ -1,26 +1,23 @@
 # include <unistd.h>
 
-
 void rstr_capitalizer(char *string)
 {
 	int index;
 
 	index = 0;
 
-	if (string[index] == ' ' || (string[index] >= 9 && string[index] <= 13))
-        {
-            write(1, &string[index], 1);
-            index++;
-        }	
+	while (string[index] == ' ' && (string[index] >= 9 && string[index] <= 13))
+		index++;
 	
 	while (string[index] != '\0')
 	{
 		if (string[index] >= 'A' && string[index] <= 'Z')
 			string[index] += 32;
 		
-		if (string[index] >= 'a' && string[index] <= 'z' && (string[index + 1] == ' ' || string[index + 1] == '\0' || string[index + 1] == '\t'))
+		if ((string[index] >= 'a' && string[index] <= 'z') && (string[index + 1] == ' ' || string[index + 1] == '\t' || string[index + 1] == '\n' || string[index + 1] == '\0'))
 			string[index] -= 32;
-		write (1, &string[index], 1);
+
+		write(1, &string[index], 1);
 		index++;
 	}
 }
@@ -29,19 +26,22 @@ int main (int argc, char **argv)
 {
 	int index;
 
-	if (argc <= 1)
+	index = 1;
+
+	if (argc < 2)
 	{
-		write (1, "\n", 1);
+		write(1, "\n", 1);
 		return (0);
 	}
 
-	index = 1;
 	while (index < argc)
 	{
 		rstr_capitalizer(argv[index]);
-		write (1, " ", 1);
+		if (index < argc - 1)
+			write (1, "\n", 1);
 		index++;
 	}
+
 	write (1, "\n", 1);
 	return (0);
 }
