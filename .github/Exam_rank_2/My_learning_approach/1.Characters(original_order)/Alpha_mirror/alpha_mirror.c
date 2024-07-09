fef09a44ad7f7b046ae0213d489816dc90c875bf
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-07-09 11:48:57 +0200

    Update alpha_mirror.c

--- .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Alpha_mirror/alpha_mirror.c
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Alpha_mirror/alpha_mirror.c
@@ -3,26 +3,29 @@
 // The objective is that the first character will become the last character, the second character will become the second to last, and so on.
 // Example: abc --> zyx
 
+
 int main (int argc, char **argv)
 {
-	int index;
 	char *string;
+	int index;
 
 	if (argc != 2)
 	{
 		write (1, "\n", 1);
 		return (0);
 	}
-	
+
 	index = 0;
 	string = argv[1];
-
+	
 	while (string[index] != '\0')
 	{
 		if (string[index] >= 'a' && string[index] <= 'z')
-			string[index] = 'a' - string[index] + 'z';
+			string[index] = ('a' - string[index]) + 'z';
+		
 		else if (string[index] >= 'A' && string[index] <= 'Z')
-			string[index] = 'A' - string[index] + 'Z';
+			string[index] = ('A' - string[index]) + 'Z';
+
 		write (1, &string[index], 1);
 		index++;
 	}
