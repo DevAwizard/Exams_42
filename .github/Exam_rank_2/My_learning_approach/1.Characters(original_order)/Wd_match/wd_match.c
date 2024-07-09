600f42e1e7a17b148a2290d079462c2688fe8dd3
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-07-09 11:39:42 +0200

    Update wd_match.c

--- .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Wd_match/wd_match.c
+++ .github/Exam_rank_2/My_learning_approach/1.Characters(original_order)/Wd_match/wd_match.c
@@ -5,71 +5,61 @@ This program checks if all characters of the first string (string_s1) appear in 
 If so, it prints the first string. Otherwise, it prints nothing.
 
 Example:
-Input: "./a.out abc abdc"
-Output: "abc"
+Input: "./a.out"abc" "abdc" | cat -e
+Output: abc$
 
-Input: "./a.out abc acb"
-Output: ""
+Input: "./a.out "abc" "acb" | cat -e
+Output: $
 
-Input: "./a.out test this is a test"
-Output: "test"
+Input: "./a.out "test" "this is a test" | cat -e
+Output: test$
 */
 
-// Function that checks if all characters of string_s1 are in string_s2 in the same order
 
-char *wd_match(char *string_s1, char *string_s2)
+# include <unistd.h>
+
+void	ft_putstr(char const *string)
 {
-	int index_s1;
-	int index_s2;
+	int	index;
 
-	index_s1 = 0;
-
-	while (string_s1[index_s1] != '\0')
+	index = 0;
+	while (string[index] != '\0')
 	{
-		index_s2 = 0;
-		
-		while (string_s2[index_s2] != '\0')
-		{
-			if (string_s1[index_s1] == string_s2[index_s2])
-			{
-				index_s1++;
-				break;
-			}
-			index_s2++;
-		}
-
-		if (string_s1[index_s1] == '\0')
-			break ;
+		write(1, &string[index], 1);
+		index++;
 	}
-
-	if (string_s1[index_s1] == '\0')
-		return (string_s1);
-	
-	return(NULL);
 }
 
 int main (int argc, char **argv)
 {
 	int index_s1;
-	char *temporal;
+	int index_s2;
 
-	if (argc != 3)
+	char *s1;
+	char *s2;
+
+	if(argc != 3)
 	{
 		write (1, "\n", 1);
 		return (0);
 	}
 
-	index_s1 = 0;
-	temporal = wd_match(argv[1], argv[2]);
 
-	if (temporal != NULL)
+	s1 = argv[1];
+	s2 = argv[2];
+	index_s1 = 0;
+	index_s2 = 0;
+
+	while (s2[index_s2] != '\0')
 	{
-		while (argv[1][index_s1] != '\0')
-		{
-			write(1, &argv[1][index_s1], 1);
+		if (s2[index_s2] == s1[index_s1])
 			index_s1++;
-		}
+		index_s2++;
 	}
+
+	if (!s1[index_s1])
+		ft_putstr(s1);
+
 	write (1, "\n", 1);
 	return (0);
 }
