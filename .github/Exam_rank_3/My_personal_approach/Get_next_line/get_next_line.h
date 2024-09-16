f7c63140a6be1e4b36cd03bcd4ba363549414649
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-09-16 15:50:36 +0200

    Create get_next_line.h

---
+++ .github/Exam_rank_3/My_personal_approach/Get_next_line/get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <fcntl.h>
+
+#endif
