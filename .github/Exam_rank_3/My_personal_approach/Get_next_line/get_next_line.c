f5c8005f34d1a0d372ccfb10f5ee7ead422a6c90
Anita <153505451+DevAwizard@users.noreply.github.com>
2024-09-18 18:34:16 +0200

    Update get_next_line.c

--- .github/Exam_rank_3/My_personal_approach/Get_next_line/get_next_line.c
+++ .github/Exam_rank_3/My_personal_approach/Get_next_line/get_next_line.c
@@ -3,74 +3,69 @@
 char *ft_strdup(char *string)
 {
 	int index;
-	char *dup;
+	char *duplicate;
 	
 	index = 0;
-
 	while (string[index])
 		index++;
-	
-	dup = malloc((index + 1) *sizeof(char));
-
-	if (!dup)
+	duplicate = malloc((index + 1) * sizeof(char));
+	if (!duplicate)
 		return (NULL);
-
 	index = 0;
-	while (string[index])
+	while(string[index])
 	{
-		dup[index] = string[index];
+		duplicate[index] = string[index];
 		index++;
 	}
-	dup[index] = '\0';
-	return (dup);
+	duplicate[index] = '\0';
+	return (duplicate);
 }
-
 
 char *get_next_line(int fd)
 {
-	static char		buffer[BUFFER_SIZE];
-	static int		buffer_index = 0;
-	static int		buffer_readed = 0;
-	char			line[70000];
-	int				index;
+	static char buffer[BUFFER_SIZE];
+	static int buffer_index = 0;
+	static int buffer_readed = 0; // es un int no un char 
+	int index;
+	char line[70000];
 
-	index = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	
+	index = 0;
 	while (1)
 	{
 		if (buffer_index >= buffer_readed)
 		{
 			buffer_readed = read(fd, buffer, BUFFER_SIZE);
 			buffer_index = 0;
-			if (buffer_readed <= 0)
+			if (buffer_readed <= 0) // Acordarse de esto
 				break ;
 		}
 		line[index++] = buffer[buffer_index++];
-		if (line[index - 1] == '\n')
+
+		if (line[index - 1] == '\n' || line[index - 1] == EOF)
 			break ;
 	}
-	line[index] = '\0';
-	if (index == 0)
+	if (index == 0) // acordarse que es index 
 		return (NULL);
+	line[index] = '\0';
 	return (ft_strdup(line));
 }
 
-int main (void)
+int main(void)
 {
-	int		fd;
-	char	*line;
+	int fd;
+	char *line;
 
-	fd = open ("test.txt", O_RDONLY);
+	fd = open("text.txt", O_RDONLY);
 	if (fd < 0)
-		return (printf("cant be opened\n"), 1);
-	while ((line = get_next_line(fd)))
+		return (printf("Fd cant be openned\n"), 1);
+
+	while ((line = get_next_line(fd))) // acordarme que son 3 parentesis
 	{
 		printf("%s", line);
 		free(line);
 	}
-	printf("\n");
 	close(fd);
 	return (0);
 }
